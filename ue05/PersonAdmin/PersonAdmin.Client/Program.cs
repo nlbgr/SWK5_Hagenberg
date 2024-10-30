aad6b74f9d0c2b68a0975797526be890c29defd6
Noah Lackenberger <noahtobias.lackenberger@gmail.com>
2024-10-30 09:05:24 +0100

    ue05: initial setup

---
+++ ue05/PersonAdmin/PersonAdmin.Client/Program.cs
@@ -0,0 +1,6 @@
+﻿static void PrintTitle(string text = "", int length = 60, char ch = '-')
+{
+  int preLen = (length - (text.Length + 2)) / 2;
+  int postLen = length - (preLen + text.Length + 2);
+  Console.WriteLine($"{new String(ch, preLen)} {text} {new String(ch, postLen)}");
+}
---
+++ ue05/PersonAdmin/PersonAdmin.Dal.Simple/SimplePersonDao.cs
@@ -0,0 +1,11 @@
+﻿namespace PersonAdmin.Dal.Simple;
+
+public class SimplePersonDao
+{
+  // private static IList<Person> personList = new List<Person>
+  // {
+    // new (id: 1, firstName: "John", lastName: "Doe",        dateOfBirth: DateTime.Now.AddYears(-10)),
+    // new (id: 2, firstName: "Jane", lastName: "Doe",        dateOfBirth: DateTime.Now.AddYears(-20)),
+    // new (id: 3, firstName: "Max",  lastName: "Mustermann", dateOfBirth: DateTime.Now.AddYears(-30))
+  // };
+}
