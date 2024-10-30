72205d96c4dd361925d56a200d2a1ecf67a51b29
Noah Lackenberger <noahtobias.lackenberger@gmail.com>
2024-10-30 11:00:53 +0100

    ue05: introduced template method for queries

---
+++ ue05/PersonAdmin/Dal.Common/AdoTemplate.cs
@@ -0,0 +1,32 @@
+﻿using System.Data.Common;
+
+namespace Dal.Common {
+    public class AdoTemplate {
+        public async Task<IEnumerable<T>> Query<T>(string commandText, RowMapper<T> rowMapper) {
+            (string connectionString, string providerName) =
+                ConfigurationUtil.GetConnectionParameters(
+                    "PersonDbConnection",
+                    "ProviderName"
+                );
+
+            DbUtil.RegisterAdoProviders();
+            DbProviderFactory dbFactory = DbProviderFactories.GetFactory(providerName);
+
+            await using DbConnection? connection = dbFactory.CreateConnection() ?? throw new InvalidOperationException("DbProviderFactory.CreateConnection() returned null");
+            connection.ConnectionString = connectionString;
+            await connection.OpenAsync();
+
+            await using DbCommand command = connection.CreateCommand();
+            command.CommandText = commandText;
+
+            await using DbDataReader reader = await command.ExecuteReaderAsync();
+
+            var items = new List<T>();
+            while (await reader.ReadAsync()) {
+                items.Add(rowMapper(reader));
+            }
+
+            return items;
+        } // Connection.DisposeAsync -> Connection.CloseAsync, ... (alle using statements werden freigegeben
+    }
+}
--- ue05/PersonAdmin/PersonAdmin.Dal.Ado/AdoPersonDao.cs
+++ ue05/PersonAdmin/PersonAdmin.Dal.Ado/AdoPersonDao.cs
@@ -1,47 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Common;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Data;
 using Dal.Common;
-using Microsoft.Data.SqlClient;
 using PersonAdmin.Dal.Interface;
 using PersonAdmin.Domain;
 
 namespace PersonAdmin.Dal.Ado {
     public class AdoPersonDao : IPersonDao {
+        private Person MapRowToPerson(IDataRecord row) {
+            return new Person(
+                id: (int)row["Id"],
+                firstName: (string)row["first_name"],
+                lastName: (string)row["last_name"],
+                dateOfBirth: (DateTime)row["date_of_birth"]
+            );
+        }
+
+        private readonly AdoTemplate template = new();
+
         public async Task<IEnumerable<Person>> FindAllAsync() {
-            (string connectionString, string providerName) =
-                ConfigurationUtil.GetConnectionParameters(
-                    "PersonDbConnection",
-                    "ProviderName"
-                );
-
-            DbUtil.RegisterAdoProviders();
-            DbProviderFactory dbFactory = DbProviderFactories.GetFactory(providerName);
-
-            await using DbConnection? connection = dbFactory.CreateConnection() ?? throw new InvalidOperationException("DbProviderFactory.CreateConnection() returned null");
-            connection.ConnectionString = connectionString;
-            await connection.OpenAsync();
-
-            await using DbCommand command = connection.CreateCommand();
-            command.CommandText = "SELECT * FROM PERSON";
-
-            await using DbDataReader reader = await command.ExecuteReaderAsync();
-
-            var items = new List<Person>();
-            while (await reader.ReadAsync()){
-                items.Add(new Person(
-                    id: (int)reader["Id"],
-                    firstName: (string)reader["first_name"],
-                    lastName: (string)reader["last_name"],
-                    dateOfBirth: (DateTime)reader["date_of_birth"]
-                    )
-                );
-            }
-
-            return items;
-        } // Connection.DisposeAsync -> Connection.CloseAsync, ... (alle using statements werden freigegeben
+            return await template.Query<Person>("SELECT * FROM PERSON", MapRowToPerson);
+        } 
     }
 }
